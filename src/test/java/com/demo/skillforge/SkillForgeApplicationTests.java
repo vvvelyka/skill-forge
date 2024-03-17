07b824eb6d332a2726e3531877197c0c32c3c386
Veronika Velyka <Veronika Velyka>
2024-03-17 15:49:02 +0200

    intial commit

---
+++ src/test/java/com/demo/skillforge/SkillForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.skillforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
