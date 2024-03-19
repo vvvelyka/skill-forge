bf069e2a28a7942b532d8144dbe184d8acd2b63b
Veronika Velyka <Veronika Velyka>
2024-03-19 11:39:33 +0200

    cahnged to multimodule structure

--- util/src/test/java/com/demo/util/UtilApplicationTests.java
+++ util/src/test/java/com/demo/util/UtilApplicationTests.java
@@ -1,13 +1,13 @@
-package com.demo.skillforge;
+package com.demo.util;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SkillForgeApplicationTests {
+class UtilApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
